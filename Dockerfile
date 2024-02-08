4411c6bf01808b6ce534219bb053b0d55b8676ff
ELFrancisco4 <offiongfrancis14@gmail.com>
2024-02-08 23:16:51 +0100

    Containerization support for sanitrack web

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# Stage 1: Build the React application
+FROM node:16-alpine as build
+
+# Set the working directory in the Docker container
+WORKDIR /app
+
+# Copy the package.json and package-lock.json (or yarn.lock) files
+COPY package*.json ./
+# If you are using yarn, copy yarn.lock and use yarn commands below
+COPY yarn.lock ./
+
+# Install dependencies
+RUN yarn install
+# For yarn, use:
+# RUN yarn install
+
+# Copy the rest of your application's code
+COPY . .
+
+# Build the application
+RUN npm run build
+
+# Stage 2: Serve the application with Nginx
+FROM nginx:stable-alpine as serve
+
+# Copy the build output to replace the default nginx contents.
+COPY --from=build /app/dist /usr/share/nginx/html
+
+# Copy nginx configuration (optional)
+# COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+# Expose port 80 to the Docker host, so we can access it 
+# from the outside.
+EXPOSE 80
+
+# Start Nginx and keep it running in the foreground
+CMD ["nginx", "-g", "daemon off;"]
--- src/views/Work History/Room Work History/RoomHistory.js
+++ src/views/Work History/Room Work History/RoomHistory.js
@@ -19,7 +19,7 @@ const RoomHistory = () => {
     const fetchRoomHistory = async () => {
       if (allRooms.length > 0 && !isFetched) {
         setLoading(true);
-        const historyPromises = allRooms.map((room) => getRoomHistory(room._id));
+        const historyPromises = allRooms.map(room => getRoomHistory(room._id));
         const histories = await Promise.all(historyPromises);
 
         const newHistory = {};
@@ -37,16 +37,19 @@ const RoomHistory = () => {
   }, [allRooms, getRoomHistory, isFetched]);
 
   return (
-    <Box>
+    <Box
+      sx={{
+        maxHeight: '600px', // Set a max height for the Box
+        overflow: 'auto' // Enable scrolling
+      }}
+    >
       <Typography variant="h3" p={2} gutterBottom>
         Room History
       </Typography>
       {loading ? (
         <Loader />
       ) : allRooms.length > 0 ? (
-        allRooms.map((room) => (
-          <HistoryDetails key={room._id} name={room.roomName} detailId={room._id} historyData={roomHistory[room._id]} />
-        ))
+        allRooms.map(room => <HistoryDetails key={room._id} name={room.roomName} detailId={room._id} historyData={roomHistory[room._id]} />)
       ) : (
         <Typography variant="body1">No rooms available</Typography>
       )}
