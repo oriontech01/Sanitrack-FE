83312bdcc6c7640efd1db0e9bbfc3a894ab0e2fd
ELFrancisco4 <offiongfrancis14@gmail.com>
2024-02-16 13:04:26 +0100

    Dashboard UI fix: Edit Landing Page Nav layout, add title to Charts component

--- src/views/Dashboard/Landing Page/index.js
+++ src/views/Dashboard/Landing Page/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Tab, Tabs, Box } from '@mui/material';
+import { Tab, Tabs, Box, Typography, Paper } from '@mui/material';
 import { Link, Outlet } from 'react-router-dom';
 
 const SanitrackLandingPage = () => {
@@ -7,21 +7,47 @@ const SanitrackLandingPage = () => {
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
+  const tabStyle = {
+    flex: 1,
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    textTransform: 'none',
+    color: 'gray', // Adjust the color to match your design
+    '&.Mui-selected': {
+      color: 'primary' // Adjust the selected tab color to match your design
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: 'rgba(100, 95, 228, 0.32)' // Adjust focus color if necessary
+    }
+  };
+
   return (
     <Box>
-      <Tabs
-        value={selectedTab}
-        onChange={handleChange}
-        style={{
-          margin: '0 auto',
-          justifyContent: 'center' // Align tabs at the center
-        }}
-      >
-        <Tab style={{ marginLeft: 200 }} label="Master Sanitation Schedule" component={Link} to="/dashboard/sanitation-schedule" />
-        <Tab label="Facility Cleaning Timer" component={Link} to="/dashboard/cleaning-timer" />
-        <Tab label="Facility Overview" component={Link} to="/dashboard/facility-overview" />
-      </Tabs>
-      <Outlet/>
+      <Box>
+        <Typography variant="h2" color="blue">
+          Welcome {localStorage.getItem('name')}!
+        </Typography>
+        <Typography variant="body1" marginTop={2}>
+          Keep track of all Cleaning Activities, Work Orders, Cleaners and Inspectors.
+        </Typography>
+      </Box>
+      <Box sx={{ width: '100%', bgcolor: 'background.paper', marginTop: 2 }}>
+        <Paper elevation={0} square>
+          <Tabs
+            value={selectedTab}
+            onChange={handleChange}
+            variant="fullWidth"
+            indicatorColor="primary" // Adjust the indicator color to match your design
+            textColor="primary"
+            centered
+          >
+            <Tab label="Facility Overview" component={Link} to="/dashboard/facility-overview" sx={tabStyle} />
+            <Tab label="Master Sanitation Schedule" component={Link} to="/dashboard/sanitation-schedule" sx={tabStyle} />
+            <Tab label="Facility Cleaning Timer" component={Link} to="/dashboard/cleaning-timer" sx={tabStyle} />
+          </Tabs>
+        </Paper>
+      </Box>
+      <Outlet />
     </Box>
   );
 };
