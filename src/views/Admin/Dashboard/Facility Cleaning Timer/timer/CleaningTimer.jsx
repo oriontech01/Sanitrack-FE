ff9c345cee7ed201ebd6357ad504bf26dc325345
achonyerichard <achonyerichard@gmail.com>
2024-03-06 09:37:28 +0100

    fixed no data error

--- src/views/Admin/Dashboard/Facility Cleaning Timer/timer/CleaningTimer.jsx
+++ src/views/Admin/Dashboard/Facility Cleaning Timer/timer/CleaningTimer.jsx
@@ -117,108 +117,87 @@ const CleaningTimer = () => {
           </div>
           <div className="">
             <div className="grid lg:grid-cols-3 gap-6 mt-4 grid-cols-1">
-            {
-              console.log(currentItems)
-            }
-              { currentItems === true ? currentItems?.map((item, i) => {
-                const newItem = item?.allTask[0];
-                console.log('secf', item);
-                return (
-                  <div className="shadow-lg" key={i}>
-                    <Link style={{ textDecoration: 'none' }} to={`/dashboard/work-order-facility/${newItem?.assigned_room?._id}`}>
-                      <div className="div-47">
-                        <SpeedometerValue
-                          cleanTime={
-                            newItem?.task_stage.toLowerCase() === 'clean'
-                              ? 25
-                              : newItem?.task_stage.toLowerCase() === 'preop'
-                                ? 60
-                                : newItem?.task_stage.toLowerCase() === 'release'
-                                  ? 90
-                                  : 0
-                          }
-                        />
+              {console.log(currentItems)}
+              {allCleanerTimer.length > 0 &&
+                currentItems?.map((item, i) => {
+                  const newItem = item?.allTask[0];
+                  console.log('secf', item);
+                  return (
+                    <div className="shadow-lg" key={i}>
+                      <Link style={{ textDecoration: 'none' }} to={`/dashboard/work-order-facility/${newItem?.assigned_room?._id}`}>
+                        <div className="div-47">
+                          <SpeedometerValue
+                            cleanTime={
+                              newItem?.task_stage.toLowerCase() === 'clean'
+                                ? 25
+                                : newItem?.task_stage.toLowerCase() === 'preop'
+                                  ? 60
+                                  : newItem?.task_stage.toLowerCase() === 'release'
+                                    ? 90
+                                    : 0
+                            }
+                          />
 
-                        <div className="div-48 capitalize">{newItem?.assigned_room?.roomName}</div>
-                        <div className="div-49">
-                          <div className="div-50">
-                            <div className="div-51">
-                              <div className="div-52" />
-                              <div className="div-53">Clean</div>
-                            </div>
-                            <div className="div-54">
-                              <div className="div-55" />
-                              <div className="div-56">Pre-Op</div>
-                            </div>
-                            <div className="div-57">
-                              <div className="div-58" />
-                              <div className="div-59">Release</div>
-                            </div>
-                          </div>
-                          <div className="div-60">Planned Time</div>
-                          <div className="div-61">
-                            <div className="div-62">
-                              <div className="div-63">Clean</div>
-                              <div className="div-64">Pre-Op</div>
-                              <div className="div-65">Release</div>
-                              <div className="div-66">Actual Time</div>
-                              <div className="div-67">Clean</div>
-                              <div className="div-68">Pre-Op</div>
-                              <div className="div-69">Release</div>
-                            </div>
-                            <div className="div-70">
-                              <div className="div-71">{ convertSecondsToHMS(newItem ?newItem?.planned_time?.clean_time: 0) ?? '-' }</div>
-                              <div className="div-72">{ convertSecondsToHMS(newItem ?newItem?.planned_time?.preOp_time:0) ?? '-'}</div>
-                              <div className="div-73">{convertSecondsToHMS(newItem ?newItem?.planned_time?.release_time :0) ?? '-'}</div>
-                              <div className="div-74">
-                                {convertMilliSecondsToHMS(
-                                  item?.actualTime
-                                    ? item?.actualTime?.clean_time.time.reduce((acc, current) => acc + Number(current), 0)
-                                    : 0
-                                )}
+                          <div className="div-48 capitalize">{newItem?.assigned_room?.roomName}</div>
+                          <div className="div-49">
+                            <div className="div-50">
+                              <div className="div-51">
+                                <div className="div-52" />
+                                <div className="div-53">Clean</div>
                               </div>
-                              <div className="div-75">
-                                {convertMilliSecondsToHMS(
-                                  item?.actualTime
-                                    ? item?.actualTime?.preOp_time.time.reduce((acc, current) => acc + Number(current), 0)
-                                    : 0
-                                )}
+                              <div className="div-54">
+                                <div className="div-55" />
+                                <div className="div-56">Pre-Op</div>
                               </div>
-                              <div className="div-76">
-                                {convertMilliSecondsToHMS(item?.actualTime ? item?.actualTime?.release_time : 0)}
+                              <div className="div-57">
+                                <div className="div-58" />
+                                <div className="div-59">Release</div>
+                              </div>
+                            </div>
+                            <div className="div-60">Planned Time</div>
+                            <div className="div-61">
+                              <div className="div-62">
+                                <div className="div-63">Clean</div>
+                                <div className="div-64">Pre-Op</div>
+                                <div className="div-65">Release</div>
+                                <div className="div-66">Actual Time</div>
+                                <div className="div-67">Clean</div>
+                                <div className="div-68">Pre-Op</div>
+                                <div className="div-69">Release</div>
+                              </div>
+                              <div className="div-70">
+                                <div className="div-71">{convertSecondsToHMS(newItem ? newItem?.planned_time?.clean_time : 0) ?? '-'}</div>
+                                <div className="div-72">{convertSecondsToHMS(newItem ? newItem?.planned_time?.preOp_time : 0) ?? '-'}</div>
+                                <div className="div-73">
+                                  {convertSecondsToHMS(newItem ? newItem?.planned_time?.release_time : 0) ?? '-'}
+                                </div>
+                                <div className="div-74">
+                                  {convertMilliSecondsToHMS(
+                                    item?.actualTime
+                                      ? item?.actualTime?.clean_time.time.reduce((acc, current) => acc + Number(current), 0)
+                                      : 0
+                                  )}
+                                </div>
+                                <div className="div-75">
+                                  {convertMilliSecondsToHMS(
+                                    item?.actualTime
+                                      ? item?.actualTime?.preOp_time.time.reduce((acc, current) => acc + Number(current), 0)
+                                      : 0
+                                  )}
+                                </div>
+                                <div className="div-76">
+                                  {convertMilliSecondsToHMS(item?.actualTime ? item?.actualTime?.release_time : 0)}
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-
-                      <div className="column-6">
-                        {newItem?.assigned_inspector.map(inspector => (
-                          <div className="div-139" key={inspector?._id}>
-                            <div className="div-140">{inspector?.username}</div>
-                            <div className="div-141">{inspector?.addressId}</div>
-                            <div className="div-142">
-                              <img
-                                alt=""
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/fe3d622b5bd936f2a023fbc2a223e744c330598e2acd6499c9c3913b5ca0a49d?"
-                                className="img-17"
-                              />
-                              <img
-                                alt=""
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/f1b34ffbd3c5f828d8a17a1e4660414999adbb811d9353f62e479d5a2c254c33?"
-                                className="img-18"
-                              />
-                            </div>
-                          </div>
-                        ))}
 
                         <div className="column-6">
-                          {newItem?.assigned_cleaner.map(cleaner => (
-                            <div className="div-139" key={cleaner?._id}>
-                              <div className="div-140">{cleaner?.username ?? '-'}</div>
-                              <div className="div-141">{cleaner?.addressId ?? '-'}</div>
+                          {newItem?.assigned_inspector.map(inspector => (
+                            <div className="div-139" key={inspector?._id}>
+                              <div className="div-140">{inspector?.username}</div>
+                              <div className="div-141">{inspector?.addressId}</div>
                               <div className="div-142">
                                 <img
                                   alt=""
@@ -235,24 +214,50 @@ const CleaningTimer = () => {
                               </div>
                             </div>
                           ))}
+
+                          <div className="column-6">
+                            {newItem?.assigned_cleaner.map(cleaner => (
+                              <div className="div-139" key={cleaner?._id}>
+                                <div className="div-140">{cleaner?.username ?? '-'}</div>
+                                <div className="div-141">{cleaner?.addressId ?? '-'}</div>
+                                <div className="div-142">
+                                  <img
+                                    alt=""
+                                    loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/fe3d622b5bd936f2a023fbc2a223e744c330598e2acd6499c9c3913b5ca0a49d?"
+                                    className="img-17"
+                                  />
+                                  <img
+                                    alt=""
+                                    loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/f1b34ffbd3c5f828d8a17a1e4660414999adbb811d9353f62e479d5a2c254c33?"
+                                    className="img-18"
+                                  />
+                                </div>
+                              </div>
+                            ))}
+                          </div>
                         </div>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              }): <Typography variant='h2'>No tasks available</Typography>}
+                      </Link>
+                    </div>
+                  );
+                })}
+
+              {allCleanerTimer.length === 0 && <Typography variant="h2">No tasks available</Typography>}
             </div>
           </div>
-          <FreePagination
-            itemsPerPage={itemsPerPage}
-            totalItems={allCleanerTimer?.length}
-            paginate={paginate}
-            previousPage={previousPage}
-            nextPage={nextPage}
-            currentPage={currentPage}
-            firstPage={firstPage}
-            lastPage={lastPage}
-          />
+          {allCleanerTimer.length > 0 && (
+            <FreePagination
+              itemsPerPage={itemsPerPage}
+              totalItems={allCleanerTimer?.length}
+              paginate={paginate}
+              previousPage={previousPage}
+              nextPage={nextPage}
+              currentPage={currentPage}
+              firstPage={firstPage}
+              lastPage={lastPage}
+            />
+          )}
         </div>
       )}
     </>
