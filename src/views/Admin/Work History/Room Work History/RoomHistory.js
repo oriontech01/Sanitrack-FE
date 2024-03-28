1085ec67b00032b6b6ae40926feb6a7c6b89f3cc
achonyerichard <achonyerichard@gmail.com>
2024-03-28 10:58:40 +0100

    kene fixes

--- src/views/Admin/Work History/Room Work History/RoomHistory.js
+++ src/views/Admin/Work History/Room Work History/RoomHistory.js
@@ -6,11 +6,10 @@ import Loader from 'component/Loader/Loader';
 // import RoomHistoryDetails from 'component/History Details/RoomHistoryDetails';
 import HistoryDetails from 'component/History Details/HistoryDetails';
 const RoomHistory = () => {
-  const { allRooms, getRoom } = useRoom();
+  const { allRooms, getRoom,isLoading } = useRoom();
   const { getRoomHistory } = useWorkHistory();
   const [roomHistory, setRoomHistory] = useState({});
   const [isFetched, setIsFetched] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getRoom();
@@ -19,7 +18,7 @@ const RoomHistory = () => {
   useEffect(() => {
     const fetchRoomHistory = async () => {
       if (allRooms.length > 0 && !isFetched) {
-        setLoading(true);
+      
         const historyPromises = allRooms.map(room => getRoomHistory(room._id));
         const histories = await Promise.all(historyPromises);
 
@@ -29,7 +28,7 @@ const RoomHistory = () => {
         });
 
         setRoomHistory(newHistory);
-        setLoading(false);
+       
         setIsFetched(true);
       }
     };
@@ -37,8 +36,7 @@ const RoomHistory = () => {
     fetchRoomHistory();
   }, []);
 
-
-  // console.log("History Data,", roomHistory)
+  console.log("History Data,", roomHistory)
   return (
     <Box
       sx={{
@@ -46,19 +44,27 @@ const RoomHistory = () => {
         overflow: 'auto' // Enable scrolling
       }}
     >
-      <Typography variant="h3" p={2} gutterBottom>
-        Room History
-      </Typography>
-      {loading ? (
-        <Loader />
-      ) : allRooms.length > 0 ? (
-        allRooms.map(room => {
-          return <HistoryDetails key={room._id} name={room.roomName} detailId={room._id} historyData={roomHistory[room._id]} />;
-        })
-      ) : (
-        <Typography variant="body1">No rooms available</Typography>
+      
+    
+      <h2 className="capitalize text-2xl font-bold text-blue-500 capitalize">  Room History</h2>
+    
+      {isLoading && (
+       <div className="flex items-center justify-center pt-5">
+       <div className="relative">
+         <div className="h-12 w-12 rounded-full border-t-4 border-b-4 border-gray-200"></div>
+         <div className="absolute top-0 left-0 h-12 w-12 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
+       </div>
+     </div>
       )}
-    </Box> 
+      {(allRooms && !isLoading) &&
+        (allRooms.length > 0 ? (
+          allRooms.map(room => {
+            return <HistoryDetails key={room._id} name={room.roomName} detailId={room._id} historyData={roomHistory[room._id]} />;
+          })
+        ) : (
+          <Typography variant="body1">No rooms available</Typography>
+        ))}
+    </Box>
   );
 };
 
